e20a335f8a29d176f5f800188e97985be8f29199
DinisMiranda <40240120@esmad.ipp.pt>
2024-12-12 14:18:47 +0000

    Update ex2.py

--- Ficha9/ex2.py
+++ Ficha9/ex2.py
@@ -1,14 +1,24 @@
 import customtkinter
 import os
 
+def calc():
+    entryAlturaValue = entryAltura.get()
+    radioVarValue = radioVar.get()
+
+    print(entryAlturaValue)
+    print(radioVarValue)
+
+    PesoIdial = (int(entryAlturaValue )- 100) - (int(entryAlturaValue) - 150) / int(radioVarValue)
+    print(PesoIdial)
+    labelPesoIdealValue.configure(text=str(PesoIdial))
 
 #Abrir app
 app = customtkinter.CTk()
 app.title("My Notepad")
 
 #Resoluçao
-appwidth = 300
-appheight = 600
+appwidth = 370
+appheight = 430
 
 screenWidth = app.winfo_screenwidth()
 screenHeight = app.winfo_screenheight()
@@ -19,22 +29,36 @@ app.geometry(f"{appwidth}x{appheight}+{int(x)}+{int(y)}")
 app.resizable(False,False)
 app.configure(fg_color = "white")
 
-#Label
-labelPaís = customtkinter.CTkLabel(app, text="Indique as suas notas.", text_color="blue", font=("Helvetica",14))
-labelPaís.place(x=80, y=5)
 
-#Textbox
-textboxNotas = customtkinter.CTkTextbox(app, width=250, height=300, border_color="grey")
-textboxNotas.place(x=30, y=30)
+#Labels
+labelAlturaEmCm = customtkinter.CTkLabel(app, text="Altura (cm):", font= ("Arial", 12), fg_color="transparent", text_color="blue");
+labelAlturaEmCm.place(x=20, y=40)         
 
-#Buttons
-buttonGuardar = customtkinter.CTkButton(app, width=250, height=50, bg_color="black", text="Guardar", text_color="blue", command=guardarTexto)
-buttonGuardar.place(x=30, y=350)
+labelGenero = customtkinter.CTkLabel(app, text="Género:", font= ("Arial", 12), fg_color="transparent", text_color="blue");
+labelGenero.place(x=20, y=80) 
 
-buttonLimpar = customtkinter.CTkButton(app, width=250, height=50, bg_color="black", text="Limpar", text_color="red",  command=limparTexto)
-buttonLimpar.place(x=30, y=420)
+labelPesoIdeal = customtkinter.CTkLabel(app, text="Peso Ideal:", font= ("Arial", 20), fg_color="transparent", text_color="red");
+labelPesoIdeal.place(x=135, y=300)
 
-buttonLer = customtkinter.CTkButton(app, width=250, height=50, bg_color="black", text="Ler bloco de notas", text_color="blue",  command=lerTexto)
-buttonLer.place(x=30, y=490)
+labelPesoIdealValue = customtkinter.CTkLabel(app, text="", font= ("Arial", 20), fg_color="transparent", text_color="red");
+labelPesoIdealValue.place(x=135, y=330)
+
+#caixas de texto
+entryAltura = customtkinter.CTkEntry(app, placeholder_text="Insira a sua altura", width=150);
+entryAltura.place(x=120, y=40)
+
+#radio buttons
+radioVar = customtkinter.StringVar(value="Masculino")
+radiobutton1 = customtkinter.CTkRadioButton(app,text="Masculino", 
+                                            variable=radioVar, value=4)
+radiobutton1.place(x=120, y=80)
+
+radiobutton2 = customtkinter.CTkRadioButton(app,text="Feminino", 
+                                            variable=radioVar, value=2)
+radiobutton2.place(x=120, y=120)
+
+#BUTTONS
+btnCalcular = customtkinter.CTkButton(app, text="Calcular",command=calc, width=330, height=80, bg_color="blue", fg_color="black")
+btnCalcular.place(x=20, y=160)
 
 app.mainloop()
